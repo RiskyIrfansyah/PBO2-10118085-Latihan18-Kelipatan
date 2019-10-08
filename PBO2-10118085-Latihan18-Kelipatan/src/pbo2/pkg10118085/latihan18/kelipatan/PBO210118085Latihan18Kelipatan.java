c692384ff223be0720b9f4b44b53fd03f0cbe470
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-10-08 21:46:15 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan18-Kelipatan/src/pbo2/pkg10118085/latihan18/kelipatan/PBO210118085Latihan18Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan18.kelipatan;
+
+/**
+ *
+ * @author Risky Irfansyah
+ * Nama     : Risky Irfansyah
+ * Kelas    : PBO2
+ * Nim      : 10118085
+  * Deskripsi Program : Kelipatan
+ */
+public class PBO210118085Latihan18Kelipatan {
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i += 3.5){
+            System.out.println(i);
+        }
+    }
+}
